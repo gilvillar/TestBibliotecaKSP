5b217c50a3d2ce796e9ee509f61192445926b50e
Gilberto Villa Rodriguez <gilvillar@hotmail.com>
2024-07-15 19:42:31 -0600

    se agregan comentarios a la caba de negocio

--- Test.BLL/AuthService.cs
+++ Test.BLL/AuthService.cs
@@ -9,6 +9,9 @@ using Test.Entities;
 
 namespace Test.BLL
 {
+    /// <summary>
+    /// Esta clase representa la logica de negocio de la autenticacion de usuarios.
+    /// </summary>
     public class AuthService : IUserService
     {
         private readonly IUserRepository _userRepository;
@@ -20,15 +23,24 @@ namespace Test.BLL
             _logger = Logger;
         }
 
+        /// <summary>
+        /// Crea un usuario.
+        /// </summary>
+        /// <param name="userName">El alias de usuario.</param>
+        /// <param name="password">La contraseña de usuario.</param>
+        /// <param name="name">El nombre real del usuario.</param>
+        /// <returns>El usuario creado.</returns>
         public async Task<User?> CreateUser(string userName, string password, string name)
         {
             User user = new User();
             try
             {
+                //validamos si los parametros traen datos o no
                 if (string.IsNullOrEmpty(userName) &&
                     string.IsNullOrEmpty(password) &&
                     string.IsNullOrEmpty(name))
                 {
+                    //generamos una excepción para registrarla en log
                     throw new Exception("Todos los campos son obligartorios");
                 }
                 else
@@ -37,19 +49,24 @@ namespace Test.BLL
                     user.Password = password;
                     user.Name = name;
 
+                    //verificamos si el usuario existe o no
                     var userExist = await GetUserByUserName(userName);
 
+                    //si el usuario existe generamos una excepcion
                     if (userExist != null)
                     {
+                        //generamos una excepción para registrarla en log
                         throw new Exception("El usuario ya existe");
                     }
 
+                    //si las validaciones pasan generamos un nuevo usuario
                     return await _userRepository.CreateUser(user);
                 }
                 
             }
             catch (Exception ex)
             {
+                //ponemos en null la variable user y generamos una excepción para registrarla en log
                 user = null;
                 _logger.LogError(ex, "Ocurrio un error al intentar crear el usuario.");
             }
@@ -57,43 +74,66 @@ namespace Test.BLL
             return user;
         }
 
+        /// <summary>
+        /// Obtiene un usuario por su nombre de usuario y contraseña, para el proceso de login.
+        /// </summary>
+        /// <param name="userName">El alias de usuario.</param>
+        /// <param name="password">La contraseña de usuario.</param>
+        /// <returns>El usuario consultado.</returns>
         public async Task<User?> GetUser(string userName, string password)
         {
+            //creamos un usuario nuevo
             User user = new User();
 
             try
             {
+                //asignamos los valores a los campos del usuario
                 user.Username = userName;
                 user.Password = password;
 
+                //realizamos la consulta del usuario
                 return await _userRepository.GetUser(user);
             }
             catch (Exception ex)
             {
+                //si ocurre un error se registra en el log
                 _logger.LogError(ex, "Ocurrio un error al intentar obtener al usuario");
             }
 
             return user;
         }
 
+        /// <summary>
+        /// Obtiene un usuario por su nombre de usuario.
+        /// </summary>
+        /// <param name="userName">El alias de usuario.</param>
+        /// <returns>El usuario consultado.</returns>
         public async Task<User?> GetUserByUserName(string userName)
         {
+            //generamos un nuevo usuario
             User? user = new User();
 
             try
             {
+                //realizamos la busqueda del usuario
                 return await _userRepository.GetUserByUserName(userName);
             }
             catch (Exception ex)
             {
+                //si ocurre un error lo mandamos al log
                 _logger.LogError(ex, "Ocurrio un error al intentar obtener al usuario");
             }
 
             return user;
         }
 
+        /// <summary>
+        /// Obtiene una lista de todos los usuarios.
+        /// </summary>
+        /// <returns>Una lista de usuarios.</returns>
         public async Task<List<User>> GetAllUsers()
         {
+            //devuelve la lista de usuarios
             return await _userRepository.GetAll();
         }
     }
--- Test.BLL/BookService.cs
+++ Test.BLL/BookService.cs
@@ -4,6 +4,9 @@ using Test.Entities;
 
 namespace Test.BLL
 {
+    /// <summary>
+    /// Esta clase representa la logica de negocio de los libros.
+    /// </summary>
     public class BookService : IBookService
     {
         readonly IBookRepository _bookRepository;
@@ -15,46 +18,63 @@ namespace Test.BLL
             _logger = logger;
         }
 
+        /// <summary>
+        /// Crea un libro.
+        /// </summary>
+        /// <param name="book">Objeto de tipo Book.</param>
+        /// <returns>El libro creado.</returns>
         public async Task<Book?> CreateBook(Book book)
         {
             Book? result = new Book();
             try
             {
-
+                //validamos que el titulo y autor estan vacios
                 if (string.IsNullOrEmpty(book.Title) && string.IsNullOrEmpty(book.Author))
                 {
+                    //si estan vacios generamos un excepcion y mandamos al log
                     throw new Exception("El titulo y autor del libro son obligatorios");
                 }
                 else
                 {
+                    //verificamos si el libro existe
                     var existe = await GetBookByTitle(book.Title);
                     if (existe != null)
                     {
+                        //si existe generamos una excepción y mandamos al log
                         result = null;
                         throw new Exception("El libro ya existe");
                     }
                 }
 
+                //si las conciiones se cumplen generamos un nuevo libro
                 result = await _bookRepository.CreateBook(book);
             }
             catch (Exception ex)
             {
+                //registramos las excepciones en el log
                 _logger.LogError(ex, "Ocurrio un error al intentar crear el libro.");
             }
 
             return result;
         }
 
+        /// <summary>
+        /// Elimina un libro.
+        /// </summary>
+        /// <param name="id">Valor que representa el id del libro.</param>
+        /// <returns>Verdadero o falso.</returns>
         public async Task<bool> DeleteBook(int id)
         {
             bool result = false;
             try
             {
+                //eliminamos un libro y retornamos el resultado
                 return await _bookRepository.DeleteBook(id);
 
             }
             catch (Exception ex)
             {
+                //si ocurre un error se registra en el log.
                 _logger.LogError(ex, "Ocurrio un error al intentar eliminar el libro.");
             }
 
@@ -62,68 +82,102 @@ namespace Test.BLL
 
         }
 
+        /// <summary>
+        /// Obtiene todos los libros
+        /// </summary>
+        /// <returns>Una lista de libros.</returns>
         public async Task<List<Book>> GetAllBooks()
         {
+            //obtenemos el listado de libros
             var books = await _bookRepository.GetAllBooks();
 
             return books;
         }
+
+        /// <summary>
+        /// Obtiene un libro por su titulo
+        /// </summary>
+        /// /// <param name="title">Valor que representa el titulo del libro.</param>
+        /// <returns>El libro consultado</returns>
+        public async Task<Book?> GetBookByTitle(string title)
+        {
+            Book? book = new Book();
+            try
+            {
+                //obtenemos el libro y lo retornamos
+                return await _bookRepository.GetBookByTitle(title);
+            }
+            catch (Exception ex)
+            {
+                //si ocurre un error se registra en el log.
+                _logger.LogError(ex, "Ocurrio un error al intentar obtener el libro");
+            }
+
+            return book;
+        }
+
+        /// <summary>
+        /// Obtiene un libro.
+        /// </summary>
+        /// <param name="id">Valor que representa el id del libro a buscar.</param>
+        /// <returns>Verdadero o falso.</returns>
 
         public async Task<Book?> GetBookById(int id)
         {
             Book? book = new Book();
             try
             {
+                //obtenemos el libro y lo retornamos
                 return await _bookRepository.GetBookById(id);
             }
             catch (Exception ex)
             {
+                //si ocurre un error se registra en el log.
                 _logger.LogError(ex, "Ocurrio un error al intentar obtener el libro");
             }
 
             return book;
 
         }
-        public async Task<Book?> GetBookByTitle(string title)
-        {
-            Book? book = new Book();
-            try
-            {
-                return await _bookRepository.GetBookByTitle(title);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Ocurrio un error al intentar obtener el libro");
-            }
 
-            return book;
-        }
-
+        /// <summary>
+        /// Actualiza los datos de un libro.
+        /// </summary>
+        /// <param name="id">Valor que representa el id del libro a actualizar.</param>
+        /// /// <param name="book">Valor que representa el libro a actualizar.</param>
+        /// <returns>Verdadero o falso.</returns>
         public async Task<bool> UpdateBook(int id, Book book)
         {
             bool result = false;
 
             try
             {
+                //validamos si el id coincide con el id del libro
                 if (id != book.Id)
                 {
+                    //en caso de que no coincidad se retorna un false y se genera una excepcion
                     result = false;
                     throw new Exception("El id del libro es incorrecto");
                 }
                 else
                 {
+                    //verificamos si el libro existe o no
                     var existe = _bookRepository.GetBookById(book.Id);
+
+                    //si el libro no existe retornamos un false y se genera una excepcion
                     if (existe == null)
                     {
                         result = false;
                         throw new Exception("El libro no existe");
                     }
 
+                    //si todo pasa bien se actualiza el libro
                     result = await _bookRepository.UpdateBook(book);
                 }
             }
             catch (Exception ex)
             {
+                //si ocurre un error se registra en el log.
                 _logger.LogError(ex, "Ocurrio un error al intentar actualizar el libro.");
             }
 
@@ -131,12 +185,19 @@ namespace Test.BLL
 
         }
 
+        /// <summary>
+        /// Realiza el prestamo de un libro.
+        /// </summary>
+        /// <param name="id">Valor que representa el id del libro a actualizar.</param>
+        /// /// /// <param name="book">Valor que representa el libro a actualizar.</param>
+        /// <returns>Verdadero o falso.</returns>
         public async Task<bool> LendBook(int id, Book book)
         {
             bool result = false;
 
             try
             {
+                //validamos si el id coincide con el id del libro
                 if (id != book.Id)
                 {
                     result = false;
@@ -144,37 +205,48 @@ namespace Test.BLL
                 }
                 else
                 {
+                    //verificamos si el libro existe o no
                     var existe = await _bookRepository.GetBookById(book.Id);
 
+                    //si el libro no existe retornamos un false y se genera una excepcion
                     if (existe == null)
                     {
                         result = false;
                         throw new Exception("El libro no existe");
                     }
-                    else if(existe.Available == 0)
+                    else if(existe.Available == 0) //verificamos si hay libros disponibles para prestar
                     {
+                        //si no hay libros disponibles se retorna falso y se genera una excepcion
                         result = false;
                         throw new Exception("No hay libros disponibles");
                     }
 
+                    //si todo pasa bien realizamos la operacion
                     result = await _bookRepository.UpdateBook(book);
                 }
-
             }
             catch (Exception ex)
             {
-                 _logger.LogError(ex, "Ocurrio un error al intentar actualizar el libro.");
+                //si ocurre un error se registra en el log.
+                _logger.LogError(ex, "Ocurrio un error al intentar actualizar el libro.");
             }
 
             return result;
         }
 
+        /// <summary>
+        /// Realiza la devolución de un libro.
+        /// </summary>
+        /// <param name="id">Valor que representa el id del libro a actualizar.</param>
+        /// /// /// <param name="book">Valor que representa el libro a actualizar.</param>
+        /// <returns>Verdadero o falso.</returns>
         public async Task<bool> ReturnBook(int id, Book book)
         {
             bool result = false;
 
             try
             {
+                //validamos si el id coincide con el id del libro
                 if (id != book.Id)
                 {
                     result = false;
@@ -182,24 +254,28 @@ namespace Test.BLL
                 }
                 else
                 {
+                    //verificamos si el libro existe o no
                     var existe = await _bookRepository.GetBookById(book.Id);
 
+                    //si el libro no existe retornamos un false y se genera una excepcion
                     if (existe == null)
                     {
                         result = false;
                         throw new Exception("El libro no existe");
                     }
-                    else if (existe.LendBooks == 0)
+                    else if (existe.LendBooks == 0) //verificamos si hay libros prestados para devolver
                     {
                         result = false;
                         throw new Exception("No hay libros prestados");
                     }
 
+                    // si todo pasa bien realizamos la operacion
                     result = await _bookRepository.UpdateBook(book);
                 }
             }
             catch (Exception ex)
             {
+                //si ocurre un error se registra en el log.
                 _logger.LogError(ex, "Ocurrio un error al intentar actualizar el libro.");
             }
 
