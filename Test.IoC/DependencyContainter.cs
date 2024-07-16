622779c234a4d9313e47cdedc56a771bc1b6152d
Gilberto Villa Rodriguez <gilvillar@hotmail.com>
2024-07-15 19:44:17 -0600

    se realizan comentarios a la capa IoC

--- Test.IoC/DependencyContainter.cs
+++ Test.IoC/DependencyContainter.cs
@@ -4,6 +4,10 @@ using Test.DAL;
 
 namespace Test.IoC
 {
+    /// <summary>
+    /// Esta clase se utiliza para crear un metodo de extension que permita el registro de servicios
+    /// del repositorio y de los servicios de negocio
+    /// </summary>
     public static class DependencyContainter
     {
         public static IServiceCollection ManagerService(this IServiceCollection services) 
